9e9653a65742b2933a4d71bd415dcbbbeae9bc64
qyfu <599769224@qq.com>
2024-04-15 00:27:16 +0800

    增加三个类型，使用infer获取函数第一个参数类型、获取promise的最终返回值类型、获取数据所有的元素类型

--- typesChallenge/returnType/index.ts
+++ typesChallenge/returnType/index.ts
@@ -1,15 +1,28 @@
 type ReturnTypeT<T> = T extends (...arge:any[])=>infer R ? R : T;
+type ReturnFunFirstParam<T> = T extends (f:infer R,...ages:any[]) => any ? R : T;
+type PromiseType<T> = T extends Promise<infer P> ? P : T;
+type ArrayTypes<T> = T extends (infer K) []? K :T;
 
-const exampleA = function(a:number){
-    return a+1;
-}
+
+const exampleA = (a: string) => Number(a + 1);
 
 let exampleAReturnType : ReturnTypeT<typeof exampleA>;
+let firstParamA :ReturnFunFirstParam<typeof exampleA>;
 
-const exampleB = function(b:number){
+const exampleB = function(b:number,b2:string){
     if(b>10){
         return b+"10more"
     }
     return b+1;
 }
 let exampleBReturnType : ReturnTypeT<typeof exampleB>;
+let firstParamB :ReturnFunFirstParam<typeof exampleB>;
+
+const promiseA = new Promise<string>(()=>{});
+const promiseB = new Promise<boolean>(()=>{});
+let promiseType:PromiseType<typeof promiseA>
+let promiseTypeB:PromiseType<typeof promiseB>
+
+
+const array  = ["12",123,true];
+let arrayType:ArrayTypes<typeof array>;
