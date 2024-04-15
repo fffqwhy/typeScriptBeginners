a16e04d0702b8171aa4d70344658752a421a2ad6
qyfu <599769224@qq.com>
2024-04-16 00:03:26 +0800

    部分只读，当第二个泛型为空，则全部只读

---
+++ typesChallenge/readonly/readonly.ts
@@ -0,0 +1,17 @@
+type MyExlude<T, K> = T extends K ? never : T;
+type MyReadonly2<T, K extends keyof T = keyof T> = { readonly [k in K]: T[k]} & { [k in MyExlude<keyof T, K>]: T[k] }
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  const todo: MyReadonly2<Todo, 'description'> = {
+    title: "Hey",
+    description: "foobar",
+    completed: false,
+  }
+  
+  todo.title = "Hello" // Error: cannot reassign a readonly property
+  todo.description = "barFoo" // Error: cannot reassign a readonly property
+  todo.completed = true // OK
