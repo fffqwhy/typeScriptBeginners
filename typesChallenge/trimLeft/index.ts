c378c5f79caf5c576c1909ea8259f9db29e1d76c
qyfu <599769224@qq.com>
2024-04-18 01:17:55 +0800

    增加trim字符串两侧空白符的类型

--- typesChallenge/trimLeft/index.ts
+++ typesChallenge/trimLeft/index.ts
@@ -1,7 +1,10 @@
 type Space = ' ' | '\n' | '\t'
 type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S
-
+type Trim<S extends string> = S extends `${Space}${infer R}`|`${infer R}${Space}` ? Trim<R> : S;
 
 const testStr = "  hello world";
-
-const TrimLeftStr:TrimLeft<typeof testStr> = "hello world";
+const testStr2 = " hello 2   ";
+type TrimLeftStr =TrimLeft<typeof testStr>;
+type TrimLeftStr2 = TrimLeft<typeof testStr2>;
+type TrimStr2 = Trim<typeof testStr2>;
+type TrimStr = Trim<typeof testStr>;
