80487f8e83f482f4284c66270077dbe5ca0858cc
qyfu <599769224@qq.com>
2024-04-20 23:49:31 +0800

    findEles code

--- typesChallenge/appealonlyOnce/onlyonce.ts
+++ typesChallenge/appealonlyOnce/onlyonce.ts
@@ -1,1 +1,14 @@
 // 只出现一次的类型
+type FindEles<T extends any[], Duplicates = never> = T extends [
+    infer F,
+    ...infer R
+  ]
+    ? F extends Duplicates
+      ? FindEles<R, Duplicates>
+      : F extends R[number]
+      ? FindEles<R, Duplicates | F>
+      : [F, ...FindEles<R, Duplicates>]
+    : [];
+
+type a = [1,2,3,4,1,2,3,1,5];
+type b = FindEles<a>;
