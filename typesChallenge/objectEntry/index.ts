98e46dbb4f05587e34c0887d603d8550fac19a90
qyfu <599769224@qq.com>
2024-05-04 00:23:31 +0800

    提供两种解决类型

--- typesChallenge/objectEntry/index.ts
+++ typesChallenge/objectEntry/index.ts
@@ -1,7 +1,12 @@
-type ObjectEntries<T> =  
+type ObjectEntries<T> =  {[K in keyof T ] -? :[K,T[K]] }[keyof T];
+type ObjectEntries2<T> = {
+    [P in keyof Required<T>]: [P, Required<T>[P] extends never ? undefined : Required<T>[P]]
+  }[keyof T]
 interface Model {
     name: string;
     age: number;
     locations: string[] | null;
   }
+
   type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
+  type modelEntries2 = ObjectEntries2<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
