b2f9f9f3f2d189b1819c510d5d9e8da8a03cccb9
qyfu <599769224@qq.com>
2024-05-03 23:58:44 +0800

    objectEntry

---
+++ typesChallenge/objectEntry/index.ts
@@ -0,0 +1,7 @@
+type ObjectEntries<T> =  
+interface Model {
+    name: string;
+    age: number;
+    locations: string[] | null;
+  }
+  type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
