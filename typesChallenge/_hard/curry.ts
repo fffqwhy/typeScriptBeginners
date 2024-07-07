7faa77a800b839d8ef02446468d875b33477cc5f
qyfu <599769224@qq.com>
2024-07-08 00:11:55 +0800

    函数柯里化

--- typesChallenge/_hard/curry.ts
+++ typesChallenge/_hard/curry.ts
@@ -1,5 +1,22 @@
-const add = (a: number, b: number) => a + b
-const three = add(1, 2)
+const add = (a: number, b: number): number => a + b;
+const three = add(1, 2);
 
-const curriedAdd = Currying(add)
-const five = curriedAdd(2)(3)
+type CurriedFunction<T extends any[], R> = T extends [infer A, ...infer Rest]
+  ? (arg: A) => CurriedFunction<Rest, R>
+  : R;
+
+function Currying2<T extends any[], R>(
+  fn: (...args: T) => R
+): CurriedFunction<T, R> {
+  return function Currying2(...args: any[]): any {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return (...next: any[]) => Currying2(...args, ...next);
+    }
+  };
+}
+
+const curriedAdd = Currying2(add);
+
+const five = curriedAdd(2)(3);
